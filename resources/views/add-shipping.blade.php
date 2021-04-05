8eda9d86d4c3e2f33d25c5f6df4cd3c6dda7a380
Tobi K <32499735+tkud04@users.noreply.github.com>
2021-04-05 18:36:02 +0100

    Update add-shipping.blade.php

--- resources/views/add-shipping.blade.php
+++ resources/views/add-shipping.blade.php
@@ -1,6 +1,6 @@
 <?php
-$title = "Add Plugin";
-$subtitle = "Install a plugin on the platform.";
+$title = "Add Shipping Info";
+$subtitle = "Add new shipping information.";
 ?>
 
 @extends('layout')
@@ -9,7 +9,7 @@ $subtitle = "Install a plugin on the platform.";
 
 
 @section('page-header')
-@include('page-header',['title' => "Plugins",'subtitle' => $title])
+@include('page-header',['title' => "Shipping",'subtitle' => $title])
 @stop
 
 
@@ -17,48 +17,32 @@ $subtitle = "Install a plugin on the platform.";
 <div class="row">
 <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
                             <div class="card">
-                                <h5 class="card-header">Add Plugin</h5>
+                                <h5 class="card-header">Add Shipping</h5>
                                 <div class="card-body">
-                                    <form action="{{url('add-plugin')}}" id="apl-form" method="post">
+                                    <form action="{{url('add-shipping')}}" id="ash-form" method="post">
 										{!! csrf_field() !!}
 										
 										<div class="row">
 										<div class="col-md-12">
 										<div class="form-group">
-                                            <label for="apl-name">Name</label>
-                                            <input id="apl-name" type="text" placeholder="Plugin name" name="name" class="form-control">
+                                            <label for="ash-name">Name</label>
+                                            <input id="ash-name" type="text" placeholder="Shipping name" name="name" class="form-control">
                                         </div>
 										</div>
 										<div class="col-md-12">
 										<div class="form-group">
-                                            <label for="apl-value">Value</label>
+                                            <label for="ash-value">Value</label>
                                              <textarea class="form-control" name="value" id="ap-value"></textarea>
                                         </div>
 										</div>
-										<div class="col-md-12">
-										<div class="form-group">
-                                            <h4>Status</h4>
-                                            <select class="form-control" name="status" id="ap-status" style="margin-bottom: 5px;">
-							                  <option value="none">Select status</option>
-								           <?php
-								            $secs = ['enabled' => "Enabled",'disabled' => "Disabled"];
-								            foreach($secs as $key => $value){
-									      	 
-								           ?>
-								              <option value="{{$key}}">{{$value}}</option>
-								           <?php
-								           }
-								           ?>
-							                </select>
-                                        </div>
-										</div>
+										
 										</div>
 										
 										
                                         <div class="row">
                                             <div class="col-sm-12 pl-0">
                                                 <p class="text-right">
-                                                    <button class="btn btn-space btn-secondary" id="apl-form-btn">Save</button>
+                                                    <button class="btn btn-space btn-secondary" id="ash-form-btn">Save</button>
                                                 </p>
                                             </div>
                                         </div>
